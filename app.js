a85d7d3264866d6214aadb5a0b945aca9fa54102
elgrantut <tute.sanchez@gmail.com>
2021-02-13 14:23:29 -0300

    Creating custom Lists

--- app.js
+++ app.js
@@ -20,11 +20,22 @@ mongoose.connect('mongodb://localhost:27017/todoListDB', {
 
 mongoose.set('useFindAndModify', false)
 
+// Schemas
+
 const itemsSchema = new mongoose.Schema({
   name: { type: String, required: [true, 'No names are specified!'] },
 })
 
 const Item = mongoose.model('item', itemsSchema)
+
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemsSchema],
+})
+
+const List = mongoose.model('list', listSchema)
+
+// Initial Items
 
 const item1 = new Item({
   name: 'Welcome to this to do List!',
@@ -48,7 +59,7 @@ const defaultItems = [item1, item2, item3]
 //   }
 // })
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   Item.find({}, (err, results) => {
     if (err) {
       console.log(err)
@@ -63,6 +74,29 @@ app.get('/', function (req, res) {
       })
     } else {
       res.render('list', { listTitle: 'Today', newListItems: results })
+    }
+  })
+})
+
+// Create a dinamic page category
+
+app.get('/:listName', (req, res) => {
+  const listName = req.params.listName
+
+  List.findOne({ name: listName }, (err, results) => {
+    if (err) {
+      console.log(err)
+    } else if (!listName) {
+      const list = new List({
+        name: listName,
+        items: defaultItems,
+      })
+      list.save()
+    } else {
+      res.render('list', {
+        listTitle: results.name,
+        newListItems: results.items,
+      })
     }
   })
 })
@@ -84,14 +118,6 @@ app.post('/', function (req, res) {
 
 app.post('/delete', (req, res) => {
   const id = req.body.delCheckbox
-  // Item.findOneAndRemove({ _id: id }, (err) => {
-  //   if (err) {
-  //     console.log(err)
-  //   } else {
-  //     console.log('success! You have deleted a list item')
-  //     res.redirect('/')
-  //   }
-  // })
   Item.findByIdAndRemove(id, (err) => {
     if (err) {
       console.log(err)
@@ -102,16 +128,12 @@ app.post('/delete', (req, res) => {
   })
 })
 
-app.get('/work', function (req, res) {
-  res.render('list', { listTitle: 'Work List', newListItems: workItems })
-})
+// Go to About page
 
-app.get('/about', function (req, res) {
+app.get('/about', (req, res) => {
   res.render('about')
 })
 
 // Listens to port 3000
 
-app.listen(3000, function () {
-  console.log('Server started on port 3000')
-})
+app.listen(3000, () => console.log('Server started on port 3000'))
